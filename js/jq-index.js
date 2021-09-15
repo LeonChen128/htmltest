5fbb5c96b0bafb78866b6ae394a52dbe2e9d90f1
陳進益 <a781282000@gmail.com>
2021-09-15 09:47:57 +0800

    🚀 部署！

---
+++ js/jq-index.js
@@ -0,0 +1,6 @@
+"use strict";/**
+ * @author      OA Wu <comdan66@gmail.com>
+ * @copyright   Copyright (c) 2015 - 2021, Lalilo
+ * @license     http://opensource.org/licenses/MIT  MIT License
+ * @link        https://www.ioa.tw/
+ */var B={v:1,test:function test(){var a=this;(function a(b){a.v+=b})(1)}};B.test();
---
+++ js/vue-index.js
@@ -0,0 +1,6 @@
+"use strict";/**
+ * @author      OA Wu <comdan66@gmail.com>
+ * @copyright   Copyright (c) 2015 - 2021, Lalilo
+ * @license     http://opensource.org/licenses/MIT  MIT License
+ * @link        https://www.ioa.tw/
+ */Load.init({data:{name:null},mounted:function mounted(){},computed:{},methods:{},template:El.render("\n    main#app.a\n      h1.icon-home => *text='\u4F60\u597D'\n      ")});
